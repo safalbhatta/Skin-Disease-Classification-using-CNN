3847dd5073b62db884e803a17f95ae93c91a595a
Safal Bhatta <safalbhatta1@gmail.com>
2025-12-26 00:40:50 +0545

    Remove unnecessary files and add requirements.txt

--- skin_project-1/skin_project/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('example/', include('apps.example_app.urls')),  # Include URLs from example_app
-]
--- skin_project/settings.py
+++ skin_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ko&54m*(bch18*kc=p)+4n3zrrkxbw2h)&1rlm%*1()r+4&sm^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
